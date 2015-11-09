62558615183b6f28001a24895b6ac7ac3515ec9f
khanhth <khanhth.evolableasia@gmail.com>
2015-11-09 13:24:04 +0700

    Initial Commit

---
+++ Deploygate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Deploygate
+//
+//  Created by Administrator on 11/9/15.
+//  Copyright © 2015 Tran Khanh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
